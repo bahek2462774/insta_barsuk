85dff50591163b9cc9ad38038636f62b1a9e4874
Ivan Shtyrlyaev <ee923925@gmail.com>
2025-04-21 01:55:19 +0200

    fix

--- index.mjs
+++ index.mjs
@@ -2,7 +2,7 @@ export const handler = async (event) => {
 	// TODO implement
 	const response = {
 		statusCode: 200,
-		body: JSON.stringify('Hello from Lambda! GH Remove full access from user'),
+		body: JSON.stringify('Hello from Lambda! GH speicifc function'),
 	};
 	return response;
 };
