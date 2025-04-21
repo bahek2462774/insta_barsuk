8ff81707bb5107ba103f8c9e4aec8ff8474e7533
Ivan Shtyrlyaev <ee923925@gmail.com>
2025-04-21 02:01:19 +0200

    fix

--- index.mjs
+++ index.mjs
@@ -2,7 +2,7 @@ export const handler = async (event) => {
 	// TODO implement
 	const response = {
 		statusCode: 200,
-		body: JSON.stringify('Hello from Lambda! GH nur update'),
+		body: JSON.stringify('Hello from Lambda! GH remove langdock-2 role'),
 	};
 	return response;
 };
