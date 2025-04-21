c9642d10dfe1a6819c46e677b52840ba16f0329d
Ivan Shtyrlyaev <ee923925@gmail.com>
2025-04-21 10:56:25 +0200

    add bot

--- index.mjs
+++ index.mjs
@@ -1,39 +1,47 @@
+import TelegramBot from 'node-telegram-bot-api';
+
+import settings from './settings.json'
+const token = settings.TELEGRAM_BOT_TOKEN
+const bot = new TelegramBot(token, { polling: false });
+let globalResolve
+
+
 exports.handler = async (event) => {
-	if (!event.body) {
-		return {
-			statusCode: 200,
-			headers: {
-				"Content-Type": "application/json"
-			},
-			body: JSON.stringify({ text: 'no body' })
-		};
+	if (event.body) {
+		try {
+			// Parse the incoming webhook payload from Telegram
+			const body = JSON.parse(event.body);
+			await new Promise((resolve) => {
+				globalResolve = resolve
+				bot.processUpdate(body)
+			})
+
+			setTimeout(() => { resolve('timeout') }, 2000)
+			return {
+				statusCode: 200,
+				headers: {
+					"Content-Type": "application/json"
+				},
+				body: JSON.stringify({ text: 'ok' })
+			};
+		} catch (error) {
+			// Handling potential errors during parsing
+			return {
+				statusCode: 400,
+				headers: {
+					"Content-Type": "application/json"
+				},
+				body: JSON.stringify({ error: error.message })
+			};
+		}
 	}
 
-	try {
-		// Parse the incoming webhook payload from Telegram
-		const body = JSON.parse(event.body);
-		// Extract the text from the Telegram message
-		const sentMessage = body.message && body.message.text ? body.message.text : "No message found";
-
-		// Create the response message
-		const responseMessage = `Hello World ${sentMessage}`;
-
-		// Return the response with a 200 status code
-		return {
-			statusCode: 200,
-			headers: {
-				"Content-Type": "application/json"
-			},
-			body: JSON.stringify({ text: responseMessage })
-		};
-	} catch (error) {
-		// Handling potential errors during parsing
-		return {
-			statusCode: 400,
-			headers: {
-				"Content-Type": "application/json"
-			},
-			body: JSON.stringify({ error: error.message })
-		};
-	}
 };
+
+bot.on('message', (msg) => {
+	const chatId = msg.chat.id;
+
+	// send a message to the chat acknowledging receipt of their message
+	bot.sendMessage(chatId, 'Received your message');
+	globalResolve('ok')
+});
