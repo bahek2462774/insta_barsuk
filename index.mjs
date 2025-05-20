b9f6d023657c750b1df226ec73a794516237992c
Ivan Shtyrlyaev <ee923925@gmail.com>
2025-05-21 01:11:52 +0200

    remove with

--- index.mjs
+++ index.mjs
@@ -1,5 +1,5 @@
 import TelegramBot from 'node-telegram-bot-api'
-import settings from './settings.json' with { type: 'json' }
+import settings from './settings.json'
 import { downloadInstagramReel } from './play.mjs'
 import fs from 'fs'
 
