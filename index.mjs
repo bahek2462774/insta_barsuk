cd66213a74cee5c57d5747ee3e29365aaf07a82f
Ivan Shtyrlyaev <ee923925@gmail.com>
2025-05-30 20:23:05 +0200

    with

--- index.mjs
+++ index.mjs
@@ -1,5 +1,5 @@
 import TelegramBot from 'node-telegram-bot-api'
-import settings from './settings.json' assert { type: "json" };
+import settings from './settings.json' with { type: "json" };
 import { downloadInstagramReel } from './play.mjs'
 import fs from 'fs'
 import path from 'path'
